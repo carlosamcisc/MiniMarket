d7f55c0c1a4ab02037059a660982bef4d527d324
Carlos May <carlosamcisc@gmail.com>
2026-06-12 18:34:22 -0600

    Se creó un view model para la vista de inicio para cargar un resumen rapido de Productos, Ventas e Ingresos totales

---
+++ MiniMarket/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniMarket.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Producto> Producto { get; set; } = new();
+        public List<Venta> Venta { get; set; } = new();
+        public List<DetalleVentum> DetalleVenta { get; set; } = new();
+        public int TotalProductos { get; set; }
+        public int TotalVenta { get;set; }
+        public int TotalIngresos { get; set; }
+    }
+}
