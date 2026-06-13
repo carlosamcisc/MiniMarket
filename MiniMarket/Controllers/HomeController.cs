ba647b23c55b508c1ceeac22df1653e13f8ede19
Carlos May <carlosamcisc@gmail.com>
2026-06-12 18:36:52 -0600

    Se modifico el metodo 'Index' del controlador 'HomeController' para que se pueda cargar los datos de un resumen rapido cuando inicie el sistema.

--- MiniMarket/Controllers/HomeController.cs
+++ MiniMarket/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MiniMarket.Models;
+using MiniMarket.Models.ViewModels;
 
 namespace MiniMarket.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MiniMarketContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MiniMarketContext context )
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel vm = new HomeViewModel();
+            ViewBag.TotalProductos = _context.Productos.Count();
+            ViewBag.TotalVentas = _context.Ventas.Count();
+            ViewBag.TotalIngresos = _context.DetalleVenta.Sum(d => d.Precio);
+            return View(vm);
         }
 
         public IActionResult Privacy()
