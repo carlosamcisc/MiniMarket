6127dfe5ba5c5b0e4096c48fef3fe197957753e9
Carlos May <carlosamcisc@gmail.com>
2026-06-13 15:43:56 -0600

    Se agrego un nuevo controlador para el mandejo de autenticaciones

--- MiniMarket/Controllers/AutenticacionController.cs
+++ MiniMarket/Controllers/AutenticacionController.cs
@@ -4,9 +4,17 @@ namespace MiniMarket.Controllers
 {
     public class AutenticacionController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
+        }
+        // Acción para cerrar sesión
+        public async Task<IActionResult> Logout()
+        {
+            //await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Redirige al login o inicio
+            return RedirectToAction("Login", "Autenticacion");
         }
     }
 }
