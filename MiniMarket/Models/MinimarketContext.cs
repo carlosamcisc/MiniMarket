f5d6e3614659a103be3602adf7fec17294d8cb52
Carlos May <carlosamcisc@gmail.com>
2026-06-12 21:19:23 -0600

    Se quito la cadena de conexion expuesta en el archivo 'MinimarketContext'

--- MiniMarket/Models/MinimarketContext.cs
+++ MiniMarket/Models/MinimarketContext.cs
@@ -29,9 +29,6 @@ public partial class MiniMarketContext : DbContext
 
     public virtual DbSet<Venta> Ventas { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-U8BCS9I;Database=miniMarket;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
